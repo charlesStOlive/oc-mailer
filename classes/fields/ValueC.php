4ee5b66f4ab4499e04f7c30a6435d26b7eb217ea
Charles <charles.stolive@gmail.com>
2020-04-03 19:12:50 +0200

    nettoyage

--- classes/fields/ValueC.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Waka\Mailer\Classes\fields;
-
-class ValueC extends BaseC
-{
-    public function __construct($model, $key, $config, $options)
-    {
-        parent::__construct($model, $key, $config, $options);
-    }
-}
