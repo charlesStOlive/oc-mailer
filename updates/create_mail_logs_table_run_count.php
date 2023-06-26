fe3595244cebd60cf4c0ea368615897ae51d36a0
Charles <charles@notilac.fr>
2023-06-26 11:37:45 +0200

    Suppresion des doublons logs

--- updates/create_mail_logs_table_run_count.php
+++ updates/create_mail_logs_table_run_count.php
@@ -15,29 +15,13 @@ class createMailLogsTableRunCount extends Seeder
     {
         $update = MailLog::where('id', '<>', null)->update(['count' => 1]);
 
-        // $latestMailLogs = \Illuminate\Support\Facades\DB::table('waka_mailer_mail_logs')
-        //     ->selectRaw('MAX(created_at) as latest_created_at')
-        //     ->groupBy('send_box_id', 'maileable_id', 'logeable_type', 'logeable_id', 'name', 'type');
+        $recordsToKeep = \Illuminate\Support\Facades\DB::table('waka_mailer_mail_logs')
+            ->select(\Illuminate\Support\Facades\DB::raw('MAX(id) as id'))
+            ->groupBy('name', 'send_box_id', 'maileable_id', 'logeable_type', 'logeable_id', 'type')
+            ->pluck('id');
 
-        // $mailLogs = Illuminate\Support\Facades\DB::table('waka_mailer_mail_logs')
-        //     ->joinSub($latestMailLogs, 'latest_logs', function ($join) {
-        //         $join->on('mail_logs.created_at', '=', 'latest_logs.latest_created_at');
-        //     })
-        //     ->select('waka_mailer_mail_logs.*', \Illuminate\Support\Facades\DB::raw('SUM(count) as count_sum'))
-        //     ->groupBy('send_box_id', 'maileable_id', 'logeable_type', 'logeable_id', 'name', 'type')
-        //     ->get();
-
-        // trace_log($mailLogs->toArray());
-
-        // $mailLogs->each(function ($group) {
-        //     $countSum = $group->sum('count');
-        //     $lastLog = $group->first();
-        //     $lastLog->count = $countSum;
-        //     $lastLog->save();
-        // });
-
-        // $mailLogs->except(0)->each(function ($log) {
-        //     $log->delete();
-        // });
+        \Illuminate\Support\Facades\DB::table('waka_mailer_mail_logs')
+            ->whereNotIn('id', $recordsToKeep)
+            ->delete();
     }
 }
